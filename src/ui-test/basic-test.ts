0f15ea3898d92dc8f18e29cc9ea35b34bffbd144
Dominik Jelinek <djelinek@redhat.com>
2020-05-19 20:55:13 +0200

    Fix AtlasMap ui tests timeout failures on RHEL
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/ui-test/basic-test.ts
+++ src/ui-test/basic-test.ts
@@ -15,19 +15,20 @@ export function basicTests() {
 		});
 
 		it('Start Command should show a notification with the correct text', async function () {
-			this.timeout(30000);
+			this.timeout(60000);
 			await startAtlasMap();
 			driver.sleep(1000);
-			const notificationStarting = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_STARTING); }, 20000) as Notification;
+			const notificationStarting = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_STARTING); }, 30000) as Notification;
 			expect(await notificationStarting.getMessage()).contains(notifications.ATLASMAP_STARTING);
-			await driver.wait(() => { return whilegetNotificationWithMessage(notifications.ATLASMAP_WAITING); }, 20000);
-			await atlasMapTabIsAccessible();
-			await new EditorView().closeEditor(views.ATLASMAP_TITLE);
+			await driver.wait(() => { return whilegetNotificationWithMessage(notifications.ATLASMAP_WAITING); }, 30000);
+			driver.sleep(1000);
+			await new EditorView().closeEditor(views.ATLASMAP_TITLE);	
 		});
 
 		it('Second Start Command should open AtlasMap window', async function () {
 			this.timeout(20000);
 			await startAtlasMap();
+			driver.sleep(1000);
 			await driver.wait(() => { return atlasMapWindowExists(); }, 10000);
 			await atlasMapTabIsAccessible();
 		});
@@ -45,6 +46,7 @@ export function basicTests() {
 		it('Second Stop Command should show a notification with the correct text', async function () {
 			this.timeout(30000);
 			await stopAtlasMap();
+			driver.sleep(1000);
 			const notification = await driver.wait(() => { return getNotificationWithMessage(notifications.ATLASMAP_UNABLE_LOCATE); }, 10000) as Notification;
 			expect(await notification.getMessage()).contains(notifications.ATLASMAP_UNABLE_LOCATE);
 		});
